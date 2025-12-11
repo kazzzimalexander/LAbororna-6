c6c325490c842c2904e9755ee9e493a5a5b28167
kazzzim1 <kazim2211@gmail.com>
2025-12-11 23:05:27 +0200

    привіт

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int x;
+	float dob = 1;
+	cout << "Введіть х:";
+	cin >> x;
+	for (int n = 1; n < 6; n++) {
+		dob = dob * tan(n * x);
+	}
+	cout << dob;
+}
+
